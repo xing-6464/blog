60e2a49ac347a67afdf4c1fd3fdce15a69dd35d1
xing-6464 <xing_6464@foxmail.com>
2023-02-02 17:02:46 +0800

    开发路由（处理 POSTData）

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
 
+// 用于处理 post data
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+
+    return promise
+}
+
 const serverHandle = (req, res) => {
     res.setHeader('Content-type', 'application/json')
 
@@ -14,28 +45,33 @@ const serverHandle = (req, res) => {
     // 解析 query
     req.query = qs.parse(url.split('?')[1])
 
-    // 处理blog路由
-    const blogData = handleBlogRouter(req, res)
-    if (blogData) {
-        res.end(
-          JSON.stringify(blogData)
-        )
-        return
-    }
+    // 处理post data
+    getPostData(req).then(postData => {
+        req.body = postData
 
-    // 处理user路由
-    const userData = handleUserRouter(req, res)
-    if (userData) {
-        res.end(
-          JSON.stringify(userData)
-        )
-        return
-    }
-
-    // 未命中路由, 返回404
-    res.writeHead(404, { "Content-type": "text/plan" })
-    res.write("404 Not Found\n")
-    res.end()
+        // 处理blog路由
+        const blogData = handleBlogRouter(req, res)
+        if (blogData) {
+            res.end(
+              JSON.stringify(blogData)
+            )
+            return
+        }
+    
+        // 处理user路由
+        const userData = handleUserRouter(req, res)
+        if (userData) {
+            res.end(
+              JSON.stringify(userData)
+            )
+            return
+        }
+        
+        // 未命中路由, 返回404
+        res.writeHead(404, { "Content-type": "text/plan" })
+        res.write("404 Not Found\n")
+        res.end()
+    })
 
 }
 
