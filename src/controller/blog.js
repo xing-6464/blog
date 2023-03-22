5261a6c8c557452a76c436ac961d0d35db9640c7
xing-6464 <xing_6464@foxmail.com>
2023-03-23 03:36:11 +0800

    修改错误

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -4,7 +4,8 @@ const { exec } = require('../db/mysql')
 const getList = (author, keyword) => {
   let sql = `select * from blogs where 1=1 `
   if (author) {
-    sql += `and author=${author}`
+    console.info(author)
+    sql += `and author='${author}' `
   }
   if (keyword) {
     sql += `and title like '%${keyword}%' `
