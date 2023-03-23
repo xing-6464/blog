e1a21d06eb4cdcaa006b37e9bb8e89a9cba3344d
xing-6464 <xing_6464@foxmail.com>
2023-03-23 12:52:12 +0800

    API对接mysql（博客详情和新建）

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -16,23 +16,25 @@ const getList = (author, keyword) => {
   return exec(sql)
 }
 
-const getDetail = (id) => {
-  // 返回假数据
-  return {
-    id: 1,
-    title: '标题A',
-    content: '内容A',
-    createTime: 1546610491112,
-    author: 'zhangsan',
-  }
+const getDetail = id => {
+  const sql = `select * from blogs where id='${id}'`
+  return exec(sql).then(rows => rows[0])
 }
 
 const newBlog = (blogData = {}) => {
   // blogData 是一个博客对象，包含title content 属性
+  const title = blogData.title
+  const content = blogData.content
+  const author = blogData.author
+  const createtime = Date.now()
 
-  return {
-    id: 3, // 新建博客插入到数据表里面的 id
-  }
+  let sql = `insert into blogs (title, content, createtime, author) values ('${title}', '${content}', ${createtime}, '${author}');`
+
+  return exec(sql).then(insertData => {
+    return {
+      id: insertData.insertId,
+    }
+  })
 }
 
 const updateBlog = (id, blogData = {}) => {
--- src/router/blog.js
+++ src/router/blog.js
@@ -17,21 +17,26 @@ const handleBlogRouter = (req, res) => {
     // const listData = getList(author, keyword)
     // return new SuccessModel(listData)
     const result = getList(author, keyword)
-    return result.then((listData) => {
+    return result.then(listData => {
       return new SuccessModel(listData)
     })
   }
 
   // 获取博客详情
   if (method === 'GET' && req.path === '/api/blog/detail') {
-    const data = getDetail(id)
+    const result = getDetail(id)
 
-    return new SuccessModel(data)
+    return result.then(data => new SuccessModel(data))
   }
 
   // 新建博客
   if (method === 'POST' && req.path === '/api/blog/new') {
-    const data = newBlog(req.body)
+    const author = 'zhangsan' // todo 假数据
+    req.body.author = author
+    const result = newBlog(req.body)
+    return result.then(data => {
+      return new SuccessModel(data)
+    })
 
     return new SuccessModel(data)
   }
