76777dddf8f971b99433d4a7330e5f88bbb74509
unknown <xingguang64@yeah.net>
2025-01-06 03:06:10 +0800

    xss攻击

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,3 +1,4 @@
+const xss = require('xss')
 const { exec } = require('../db/mysql')
 
 const getList = (author, keyword) => {
@@ -21,7 +22,7 @@ const getDetail = id => {
 
 const newBlog = (blogData = {}) => {
   // blogData 是一个博客对象，包含title content, author 属性
-  const title = blogData.title
+  const title = xss(blogData.title)
   const content = blogData.content
   const author = blogData.author
   const createtime = Date.now()
