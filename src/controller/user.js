b0f384513de2f57b9cb984e23e346cdca9e5f8e5
unknown <xingguang64@yeah.net>
2025-01-06 08:22:01 +0800

    密码加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,12 @@
 const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/crypto')
 
 const login = (username, password) => {
   // 防止sql注入攻击
   username = escape(username)
+
+  // 密码加密
+  password = genPassword(password)
   password = escape(password)
 
   const sql = `select username, password from users where username=${username} and password=${password};`
